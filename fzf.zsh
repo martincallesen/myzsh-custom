4005f26bbb718cbde03412aa33f9677704efb3e7
Martin Callesen <mac@nine.dk>
2023-06-26 10:15:43 +0200

    adding intellij an fzf scripts

---
+++ fzf.zsh
@@ -0,0 +1,6 @@
+# fzf configurations
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+# Uncomment the following line to disable fuzzy completion
+# export DISABLE_FZF_AUTO_COMPLETION="true"
+# Uncomment the following line to disable key bindings (CTRL-T, CTRL-R, ALT-C)
+# export DISABLE_FZF_KEY_BINDINGS="true"
