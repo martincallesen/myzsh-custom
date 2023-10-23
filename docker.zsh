52f5fa413423ca8af7bac37d0dc05608a27443f1
Martin Callesen <mac@nine.dk>
2023-10-23 12:38:25 +0200

    chore: feature added chatgpt

--- docker.zsh
+++ docker.zsh
@@ -1,5 +1,5 @@
 #List storage usage of docker
-alias docker-df='docker system df -v'
+alias ddf='docker system df -v'
 
 #Clean up docker storage
-alias docker-cl='docker system prune'
+alias dcl='docker system prune'
