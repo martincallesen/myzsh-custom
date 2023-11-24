180a0de0bc13bf1ce81d60b8eb5e93031b86713a
Martin Callesen <mac@nine.dk>
2023-11-24 07:21:43 +0100

    feature: added keycloak alias

--- keycloak.zsh
+++ keycloak.zsh
@@ -1,2 +1,4 @@
 export KEYCLOAK_HOME=~/repositories/stil/stillogin/docker/sso/base/rh-sso-7.6
-export PATH="${PATH}:~/repositories/stil/support-tools/bin"
+export PATH="${PATH}:~/repositories/stil/support-tools/bin:${KEYCLOAK_HOME}/bin"
+
+alias kcadm=kcadm.sh
