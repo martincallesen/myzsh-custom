0274aee451cfafb72c276ec206b13688deee76fb
Martin Callesen <mac@nine.dk>
2023-12-07 05:14:46 +0100

    chore: added exa

---
+++ exa.zsh
@@ -0,0 +1,9 @@
+# general use aliases
+alias ls='exa' # just replace ls by exa and allow all other exa arguments
+alias l='ls -lbF' #   list, size, type
+alias ll='ls -la' # long, all
+alias llm='ll --sort=modified' # list, long, sort by modification date
+alias la='ls -lbhHigUmuSa' # all list
+alias lx='ls -lbhHigUmuSa@' # all list and extended
+alias tree='exa --tree' # tree view
+alias lS='exa -1' # one column by just names
