f73dcf867fd350807acffdb4ece06faf79254096
Martin Callesen <mail@martincallesen.dk>
2026-05-15 21:55:40 +0200

    chore: remove keycloak.zsh and vscode.zsh aliases

--- keycloak.zsh
+++
@@ -1,4 +0,0 @@
-export KEYCLOAK_HOME=~/repositories/stil/stillogin/docker/sso/base/rh-sso-7.6
-export PATH="${PATH}:~/repositories/stil/support-tools/bin:${KEYCLOAK_HOME}/bin"
-
-alias kcadm=kcadm.sh
--- vscode.zsh
+++
@@ -1,1 +0,0 @@
-alias vscode="/Applications/Visual\ Studio\ Code.app/contents/Resources/app/bin/code"
